01e9a88c9ececfc65fccb3c3a8a865078915b817
bebekim <yh.kim@outlook.com>
2013-10-06 03:39:03 +0900

    Learn to Program
    Following the book by Chris Pine

---
+++ example.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, what is your first name?'
+firstname = gets.chomp
+puts 'and your last name is?'
+lastname = gets.chomp
+name = lastname + ', ' + firstname
+puts 'Your name is ' + name + '?'
+puts 'Pleased to meet you, ' + name + '.'
---
+++ string.rb
@@ -0,0 +1,8 @@
+var1 = 2
+var2 = '5'
+puts var1 + var2.to_i
+
+puts '5 is my favorite number'.to_i
+puts 'who asked you about 5 or whatever'.to_i
+puts ''
+
