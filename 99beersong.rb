adc0f2a6f9624b67760112a9ae00c187cba13260
bebekim <yh.kim@outlook.com>
2013-10-26 23:53:03 +0900

    99 beersong
    99beersong.rb should be designed more efficiently. Too much repetition
    of code at this point.

---
+++ 99beersong.rb
@@ -0,0 +1,27 @@
+while true
+  # refill beercount up to 99
+  beercount = 99
+  while beercount > 0
+    # important to convert beercount to string in order to writing along with string 
+    puts beercount.to_s + ' bottles of beer on the wall, ' + beercount.to_s + ' bottles of beer'
+    if beercount - 1 == 0
+      # 1 -> 0  
+      puts 'Take one down and pass it around, no more bottles of beer on the wall.'   
+    else
+      # 99 -> 1  
+      puts 'Take one down and pass it around, ' + (beercount-1).to_s + ' bottles of beer on the wall'
+    end
+    beercount = beercount - 1
+  end
+  puts '(Is this beers on the wall song annoying to you?)'
+  input = gets.chomp
+  if (input == 'yes')
+    # avoid breaking out of while loop
+    puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+  else
+    # break out of while loop
+    puts 'Ok then.' 
+    break 
+  end    
+end
