f4ebb272a2a8a394e8eaf80e56f810cf956a63e1
bebekim <yh.kim@outlook.com>
2013-10-26 16:18:40 +0900

    iterations

--- arraysanditerators.rb
+++ arraysanditerators.rb
@@ -4,3 +4,25 @@
 flavor = 'vanilla'
 [89.0, flavor, [true, false]] #variables are not objects so this array is POINTING to a float, string, and an array
 
+languages = ['English', 'Korean', 'Ruby']
+languages.each do |lang|
+  puts 'I love ' + lang + '!'
+  puts 'Don\'t you?'
+end
+puts 'And let\'s hear it for Java'
+puts '<crickets chirp in the distance>'
+puts ' '
+3.times do
+  puts 'Hip-Hip-Hooray'
+end
+puts
+
+foods = ['artichoke', 'brioche', 'caramel']
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8) '
+puts
